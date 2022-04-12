c8879de59d8e552100f38de565e5c2404b1040eb
dylanmcfarlin <dylanmcfarlin10@gmail.com>
2022-04-12 10:57:27 -0700

    CreateAccountScreen

--- Screens/CreateAccountScreen.tsx
+++ Screens/CreateAccountScreen.tsx
@@ -1,1 +1,25 @@
-//this is the createAccount screen
+import { FC } from 'react';
+import { Button } from 'react-native';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+    Home: undefined; //means route doesnt have params
+    Profile: { name : string };
+    Login: { name: string }
+    CreateAccount: undefined,
+    Loading: undefined,
+    Introduction: undefined
+};
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+const CreateAccountScreen : FC<Props> = ({ navigation }) => {
+    return (
+        <Button
+            title="Go to Jane's profile"
+            onPress={() => navigation.navigate('Profile', { name: 'Jane' })}
+        />
+    );
+};
+
+export default CreateAccountScreen;
