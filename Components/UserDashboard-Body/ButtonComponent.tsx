195c5ac804fd2b788f07692c11438835973e5cfa
sophieespejo <sophmeas@gmail.com>
2022-04-15 13:57:37 -0700

    got new MWG to get created

--- Components/UserDashboard-Body/ButtonComponent.tsx
+++ Components/UserDashboard-Body/ButtonComponent.tsx
@@ -1,27 +1,34 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { FC } from "react";
 import { StyleSheet, Text, View, Image, TextInput, Pressable } from "react-native";
 import headerLogo from "../../assets/headerLogo.png";
 import MovieClipper from "../../assets/MovieClipper.png";
 import { useFonts, Raleway_400Regular } from '@expo-google-fonts/raleway';
 import AppLoading from 'expo-app-loading';
-import {useNavigation} from '@react-navigation/native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
 
-type RootStackParamList = {
-  Home: undefined; //means route doesnt have params
-  UserDashboard: { name : string };
-  Login: { name: string }
-  CreateAccountScreen: undefined,
-  Loading: undefined,
-  Introduction: undefined,
-  NewMWGName: undefined,
-  MemberSearch: undefined
+
+interface IButtonComponent {
+  username: string,
+  userId: number
 }
 
+  type RootStackParamList = {
+      Home: undefined; //means route doesnt have params
+      Profile: { name: string };
+      Login: { username: string, userId: number },
+      CreateAccount: undefined;
+      Loading: undefined;
+      Introduction: undefined;
+      UserDashboard: { username: string, userId: number };
+      InvitationSent: undefined;
+      MemberSearch: { username: string, userId: number },
+      NewMWGName: { username: string, userId: number },
+    };
+  
+  type Props = NativeStackScreenProps<RootStackParamList, "UserDashboard">;
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
-
-const ButtonComponent: FC = () => {
+const ButtonComponent: FC<Props> = ({username, userId}) => {
   let [fontsLoaded] = useFonts({
     Raleway_400Regular,
   });
@@ -30,13 +37,17 @@ const ButtonComponent: FC = () => {
     return <AppLoading />;
   }
 
+  const navigation = useNavigation();
+
   // const navigation = useNavigation();
 
 
   return (
     <View style={{flex:1, marginTop:'5%', marginBottom: '3%', alignItems:'center'}}>
-      <Pressable style={{width:'90%', marginBottom:'3%'}} >
+      <Pressable style={{width:'90%', marginBottom:'3%'}} onPress={() => navigation.navigate('NewMWGName',{ username: username, userId: userId})}>
         <View style={styles.wgButton}>
+          <Text>{username}</Text>
+          <Text>{userId}</Text>
           <Image source={MovieClipper}></Image>
           <Text style={{color:'#383333', fontSize:20, paddingLeft:60, justifyContent:'center', textAlign:'center', fontFamily:'Raleway_400Regular'}}>Create new {"\n"}Watch Group</Text>
         </View>
---
+++ interfaces/ICreateAccountDTO.tsx
@@ -0,0 +1,5 @@
+export default interface ICreateAccountDTO{
+    Id:number,
+    Username:string,
+    Password:string
+}
