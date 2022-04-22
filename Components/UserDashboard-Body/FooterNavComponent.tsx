4a3417687ed45892b8811bd209b93dc9781a92ac
sophieespejo <sophmeas@gmail.com>
2022-04-22 10:03:05 -0700

    updated footer with right params

--- Components/UserDashboard-Body/FooterNavComponent.tsx
+++ Components/UserDashboard-Body/FooterNavComponent.tsx
@@ -18,12 +18,14 @@ import { useNavigation } from '@react-navigation/native';
 
 type RootStackParamList = {
   Home: undefined; //means route doesnt have params
-  Profile: { name: string };
-  Login: { name: string };
+  Login: undefined,
   CreateAccount: undefined;
   Loading: undefined;
   Introduction: undefined;
   UserDashboard: undefined;
+  InvitationSent: undefined;
+  MemberSearch: { newMWGname: string },
+  NewMWGName: undefined,
 };
 
 type Props = NativeStackScreenProps<RootStackParamList, "UserDashboard">;
