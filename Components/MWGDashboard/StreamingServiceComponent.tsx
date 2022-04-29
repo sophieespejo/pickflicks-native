f2c0c3b796e22aee5926708274a240c46871b5fc
sophieespejo <sophmeas@gmail.com>
2022-04-28 22:06:11 -0700

    updated streaming page

--- Components/MWGDashboard/StreamingServiceComponent.tsx
+++ Components/MWGDashboard/StreamingServiceComponent.tsx
@@ -1,7 +1,6 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import { FC, useState } from "react";
 import { StyleSheet, Text, View, Pressable, ImageBackground} from "react-native";
-import headerLogo from "../../assets/headerLogo.png";
 import { useFonts, Raleway_400Regular } from '@expo-google-fonts/raleway';
 import AppLoading from 'expo-app-loading';
 import { Button } from "react-native-paper";
@@ -18,7 +17,6 @@ import image from '../../assets/black.jpg'
 
 const StreamingServiceComponent: FC = () => {
     const navigation = useNavigation<any>();
-    //const image = { uri: "https://reactjs.org/logo-og.png" };
 
     const [streamingService, setStreamingService] = useState<Array<object>>([
       {
@@ -60,11 +58,9 @@ const StreamingServiceComponent: FC = () => {
   return (
       <View style={{flex: 1, alignItems:'center'}}>
         <View style={{ flex: 1, backgroundColor:'#DC1B21C4', borderRadius:30, width:'92%', marginTop:'8%',marginBottom:'8%', justifyContent:'center'}}>
-           
-            <View style={{flex:0.8}}>
+            <View style={{flex:0.6}}>
                 <Text style={styles.titleTxt}>Select Your  {'\n'} Streaming Service</Text>
             </View>
-
             <View style={{flex:3, alignItems:'center', width:'100%', justifyContent:'center', borderRadius: 20}}>
             {
               streamingService.map((service:any, i:number) => {
@@ -72,7 +68,7 @@ const StreamingServiceComponent: FC = () => {
                   <Pressable key={i} onPress={() => selectHandler(service.value)} style={ service.value === value ? styles.selected : styles.unselected}>
                     <ImageBackground 
                       source={service.value === value ? service.source : image}  
-                      style={styles.image}>
+                      style={service.value === value ? styles.image1 : styles.image2}>
                         <Text style={styles.option} > {service.value === value ? "" : service.label}</Text>
                     </ImageBackground>
                 </Pressable>
@@ -80,22 +76,26 @@ const StreamingServiceComponent: FC = () => {
               })
             }
             </View>
-
             <View style={{flexDirection:'row'}}>
-
               <View style={[{ flex:0.5, alignItems: "center", alignItems:'flex-start'}]}>
-          <Button uppercase={false} title="button" color='#FFFFFF' mode="text" onPress={() => {navigation.navigate()}}>
-              <Text style={styles.nextBtn}> {'\<'} Cancel </Text>
-          </Button>
+                <Button 
+                  uppercase={false} 
+                  color='#FFFFFF' 
+                  mode="text" 
+                  onPress={() => {navigation.navigate()}}>
+                    <Text style={styles.nextBtn}> {'\<'} Cancel </Text>
+                </Button>
               </View>
               <View style={[{ flex:0.5, alignItems: "center", alignItems:'flex-end'}]}>
-          <Button uppercase={false} title="button" color='#FFFFFF' mode="text" onPress={() => {navigation.navigate()}}>
-              <Text style={styles.nextBtn}>Next {'\>'}</Text>
-          </Button>
+                <Button 
+                  uppercase={false} 
+                  color='#FFFFFF' 
+                  mode="text" 
+                  onPress={() => {navigation.navigate()}}>
+                    <Text style={styles.nextBtn}>Next {'\>'}</Text>
+                </Button>
               </View>
             </View>
-
-
         </View>
       </View>
   );
@@ -108,11 +108,6 @@ const styles = StyleSheet.create({
       textAlign:'center',
       marginTop:'4%',
       color: '#FFFFFF',
-  },
-  Dropdown:{
-      borderRadius:25,
-      width:'90%',
-      fontFamily:'Raleway_400Regular'
   },
   nextBtn:{
     fontFamily: "Raleway_400Regular",
@@ -150,13 +145,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  image: {
+  // createImageBitmap: {
+  //   flex: 1,
+  //   width: '100%',
+  //   justifyContent: "center",
+  //   resizeMode:'cover',
+  //   borderRadius: 15,
+  //   overflow: 'hidden',
+  //   borderColor: 'white',
+  //   borderWidth: 3
+  // },
+  image1: {
     flex: 1,
     width: '100%',
     justifyContent: "center",
-    resizeMode:'stretch',
-    borderRadius: 10,
-    overflow: 'hidden'
+    resizeMode:'cover',
+    borderRadius: 15,
+    overflow: 'hidden',
+    borderColor: 'goldenrod',
+    borderWidth: 3
+  },
+  image2: {
+    flex: 1,
+    width: '100%',
+    justifyContent: "center",
+    resizeMode:'cover',
+    borderRadius: 15,
+    overflow: 'hidden',
+    borderColor: 'white',
+    borderWidth: 3
   },
 });
 
