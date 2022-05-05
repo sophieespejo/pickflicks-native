3c2e0a23769a01d2e7401f32abd0f06fc9294723
sophieespejo <sophmeas@gmail.com>
2022-05-05 09:06:35 -0700

    added notes on WFY component

--- Components/UserDashboard-Body/WaitingForYouComponent.tsx
+++ Components/UserDashboard-Body/WaitingForYouComponent.tsx
@@ -110,85 +110,98 @@ const WaitingForYouComponent: FC = () => {
       
       {
 
+      //all MWGstatus that the user has, this correlates with whatever MWG user is in  
       allMWG.map((group:any, i:number) => 
       
-      { 
-        if(allFaveMWG.includes(parseInt(group.mwgId)) && !group.isDeleted && group.userDoneWithGenreRankings == false && group.userDoneWithSwipes == false)
+      { //checks to see if its in the favorite list       
+        //check to see if not deleted
+        //check to see if isStarted is false -> indicates that admin hasn't started process yet, so have to wait for them
 
-      {
-        return (
-          <>
-               <Pressable key={group.id} style={[styles.wgButton, {flex:1, marginTop:'5%'}]} onPress={()=> handlePress(group.mwgName, group.mwgId)}>
-                 <View >
-                   <View  style={{paddingTop:'3%',flexDirection:'row', justifyContent:'center'}}>
-                     <Text
-                      style={{
-                        color: "#FFFFFF",
-                        fontSize: 28,
-                        fontWeight: 'bold',
-                        // justifyContent: "center",
-                        // textAlign: "center",
-                        fontFamily:'Raleway_400Regular', 
-                        marginBottom: 0,
-                      }}
+        //if isStarted is true then check to see if user is done with genre ranking
+        //if userdonewithgenreranking is false AND isStarted is true -> must go in waiting for you
+        //if userdonewithgenreranking is true AND endpoint that returns if all other members are done is false, then must be in waiting for others component
+
+        //if isStarted is true AND userdonewithgenreranking is true AND all other members are done and returned true -> must go to waiting for you component
+
+        //if isStarted is true AND userdonewithswipes is true AND bool that returns if other members are done swiping is false -> waiting for others
+
+        //if isStarted is true AND userdonewithswipes is true AND everyone else is done -> waiting for you (click to see the highest movie!)
+
+        //reset all values after the process is done (isStarted, userisdoneswipes, userisdonegenre) --> MWG should move back to waiting for others since admin hasn't started
+
+        //ugh gotta think about how it would look for admin... if groupCreatorId == userId AND isStarted is false then place in waiting for you component
+        if(allFaveMWG.includes(parseInt(group.mwgId)) && !group.isDeleted && group.isStarted == false)
+        {
+          return (
+            <>
+                <Pressable key={group.id} style={[styles.wgButton, {flex:1, marginTop:'5%'}]} onPress={()=> handlePress(group.mwgName, group.mwgId)}>
+                  <View >
+                    <View  style={{paddingTop:'3%',flexDirection:'row', justifyContent:'center'}}>
+                      <Text
+                        style={{
+                          color: "#FFFFFF",
+                          fontSize: 28,
+                          fontWeight: 'bold',
+                          // justifyContent: "center",
+                          // textAlign: "center",
+                          fontFamily:'Raleway_400Regular', 
+                          marginBottom: 0,
+                        }}
+                        >
+                        {group.mwgName}
+                      </Text>
+                    </View>
+                    <View>
+                      <Text
+                        style={{
+                          paddingTop:'2%',
+                          color: "#FFFFFF",
+                          fontSize: 20,
+                          justifyContent: "center",
+                          textAlign: "center",
+                          fontFamily:'Raleway_400Regular', 
+                          marginBottom: '10%'
+                        }}
                       >
-                      {group.mwgName}
-                    </Text>
-                  </View>
-                  {/* <View>
-                    <Avatar.Group _avatar={{
-                        size: "lg"
-                      }} max={3}>
-                        <Avatar bg="green.500" source={{
-                        uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-                      }}>
-                        AJ
-                      </Avatar>
-                      <Avatar bg="cyan.500" source={{
-                      uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-                    }}>
-                        TE
-                        </Avatar>
-                    </Avatar.Group>
-                  </View> */}
-                  <View>
-                    <Text
-                      style={{
-                        paddingTop:'2%',
-                        color: "#FFFFFF",
-                        fontSize: 20,
-                        justifyContent: "center",
-                        textAlign: "center",
-                        fontFamily:'Raleway_400Regular', 
-                        marginBottom: '10%'
-                      }}
-                    >
-                      {group.membersNames}
-                    </Text>
-                  </View>
-                  
-                  <View>
-                    <Text
-                      style={{
-                        color: "#FFFFFF",
-                        fontSize: 20,
-                        justifyContent: "center",
-                        textAlign: "center",
-                        fontFamily:'Raleway_400Regular', 
-                        marginBottom: '7%'
-                      }}
-                    >
-                        You need to start ranking genres/swiping movies!
-                    </Text>
-                  </View>
-              </View>
-              <Pressable style={styles.heart} onPress={()=>handleRemoveFavoriteMWG(group.mwgId)}>
-                <Image  source={filledHeart} ></Image>
+                        {group.membersNames}
+                      </Text>
+                    </View>
+                    
+                    <View>
+                      <Text
+                        style={{
+                          color: "#FFFFFF",
+                          fontSize: 20,
+                          justifyContent: "center",
+                          textAlign: "center",
+                          fontFamily:'Raleway_400Regular', 
+                          marginBottom: '7%'
+                        }}
+                      >
+                          You need to start ranking genres/swiping movies!
+                      </Text>
+                    </View>
+                </View>
+                <Pressable style={styles.heart} onPress={()=>handleRemoveFavoriteMWG(group.mwgId)}>
+                  <Image  source={filledHeart} ></Image>
+                </Pressable>
               </Pressable>
-            </Pressable>
-          </>
-        )
-      }})
+            </>
+          )
+        }
+
+        //if isStarted is true then check to see if user is done with genre ranking
+        //if userdonewithgenreranking is false AND isStarted is true -> must go in waiting for you
+        //if userdonewithgenreranking is true AND endpoint that returns if all other members are done is false, then must be in waiting for others component
+        if(allFaveMWG.includes(parseInt(group.mwgId)) && !group.isDeleted && group.isStarted == true && group.userDoneWithGenreRankings == false && areAllMembersDoneWithGenre == false)
+        //should areAllMembersDoneWithGenre be another property on the MWGStatus model
+        //can we link the endpoint i made in the backend to it somehow?
+        {
+          return (
+
+          )
+        }
+    })
       }
 
       {
